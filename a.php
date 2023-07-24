489e04f19e128c60d4a0c1ec589fc743e7778d91
i347higa <138637584+i347higa@users.noreply.github.com>
2023-07-24 15:13:51 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
+    //xyを含む単語を表示し、かつ()!==falseとすることで、xyを頭にもつ単語も漏れなく表示できるようにした。
     if(strpos($line, "xy")!== false ){
       echo "$line";
     }
